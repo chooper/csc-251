0833f9be7db2a9f41ffa5222e61d18fb7fdefbdb
Charles Hooper <charles.hooper@dotcloud.com>
2012-09-29 01:10:00 -0400

    Add more test coverage for HTTPRequests

--- httpd/tests/test_requests.py
+++ httpd/tests/test_requests.py
@@ -3,6 +3,61 @@ import unittest
 from cStringIO import StringIO
 from http.request import HTTPRequest
 
+
+class TestHTTPRequestForUnsupportedMethod(unittest.TestCase):
+
+    test_addr = ('127.0.0.1', 1025)
+    request = (
+        "FAIL / HTTP/1.1\r\n"
+        "User-Agent: unit test"
+        "Host: localhost:7777"
+        "Accept: */*"
+    )
+
+    def testBadProtocol(self):
+        buf = StringIO(self.request)
+        req = HTTPRequest(buf, self.test_addr)
+        result = req.handle()
+        self.assertIsNotNone(result)  # make sure no errors are returned
+        self.assertEqual(result[0], 405)
+
+
+class TestHTTPRequestForBadProtocol(unittest.TestCase):
+
+    test_addr = ('127.0.0.1', 1025)
+    request = (
+        "GET /../ FAILTHIS/1.1\r\n"
+        "User-Agent: unit test"
+        "Host: localhost:7777"
+        "Accept: */*"
+    )
+
+    def testBadProtocol(self):
+        buf = StringIO(self.request)
+        req = HTTPRequest(buf, self.test_addr)
+        result = req.handle()
+        self.assertIsNotNone(result)  # make sure no errors are returned
+        self.assertEqual(result[0], 505)
+
+
+class TestHTTPRequestForDirTraversal(unittest.TestCase):
+
+    test_addr = ('127.0.0.1', 1025)
+    request = (
+        "GET /../ HTTP/1.1\r\n"
+        "User-Agent: unit test"
+        "Host: localhost:7777"
+        "Accept: */*"
+    )
+
+    def testDirTraversal(self):
+        buf = StringIO(self.request)
+        req = HTTPRequest(buf, self.test_addr)
+        result = req.handle()
+        self.assertIsNotNone(result)  # make sure no errors are returned
+        self.assertEqual(result[0], 403)
+
+
 class TestHTTPRequestForIndex(unittest.TestCase):
 
     test_addr = ('127.0.0.1', 1025)
