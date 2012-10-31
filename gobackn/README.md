14da5a5c48f09a61f80b41ca189e71be8eac9913
Charles Hooper <charles.hooper@dotcloud.com>
2012-10-31 16:00:27 -0400

    Add code for Go-Back-N simulation

---
+++ gobackn/README.md
@@ -0,0 +1,8 @@
+Go-Back-N Simulation
+====================
+
+Work-in-progress that's supposed to impement to Go-Back-N protocol via a
+simulator. Currently, I'm about halfway between alternating-bit and
+Go-Back-N. I lost alot of time trying to figure out how to get this to
+compile in Linux (and also my last C++ class was 7 years ago).
+
---
+++ gobackn/altbit.h
@@ -0,0 +1,14 @@
+int init(void);
+float jimsrand(void);
+void generate_next_arrival(void);
+void insertevent( struct event *p);
+void stoptimer(int AorB);
+void starttimer(int AorB,float increment);
+void tolayer3(int AorB,struct pkt packet);
+void tolayer5(int AorB,char datasent[]);
+int calc_checksum(struct pkt *tgt_pkt);
+bool pkt_checksum_valid(struct pkt *tgt_pkt);
+struct pkt *make_pkt(int seqnum, char data[20]);
+void send_pkt(int caller, struct pkt *pkt_to_send);
+void send_nack(int caller, struct pkt *pkt_to_nack);
+void send_ack(int caller, struct pkt *pkt_to_ack);
